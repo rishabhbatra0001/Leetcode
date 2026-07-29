e96bfb3ed50afcd6d55ebda432928159f63ed7ec
Rishabh Batra <rishabhbatra0001@gmail.com>
2026-07-29 22:03:29 +0530

    Time: 2 ms (41.65%), Space: 7.9 MB (23.36%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        int n = sqrt(c);
-        int A[n + 1];
-        for (int i = 0 ; i <= n ; i++){
-            A[i] = i;
-        }
-
-        long long a = 0 , b = n;
+        long long a = 0 , b = sqrt(c);
         while (a <= b){
-            long long sum = 1LL * A[a] * A[a] + 1LL * A[b] * A[b];
+            long long sum = a*a + b*b;
             if (sum == c)return true;
-            if (sum < c)a++;
-            else if(sum > c) b--;
+            else if (sum < c)a++;
+            else b--;
         }
         return false;
     }
